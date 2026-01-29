a264d78b73abf85f232866bcafadb45b3b799e08
Pedro <pedro.esperidiao@gmail.com>
2026-01-29 16:28:02 +0100

    CFFI: added first CFFI-based test; fixes in C needed

--- c/cffi_load.py
+++ c/cffi_load.py
@@ -1,17 +1,20 @@
+import os
 from pathlib import Path
 import cffi
+
+# Compile the C FIFO DLL
+C_DIR = Path(__file__).parent
+assert C_DIR.is_dir(), f"C directory not found: {C_DIR}"
+BATCH_FILE = (C_DIR / "build_dll.bat").resolve()
+os.system(str(BATCH_FILE))
 
 # Get paths to CDEF and DLL files
 CDEF_FILE = Path(__file__).parent / "fifo.h"
 DLL_FILE = Path(__file__).parent / "bin/fifo.dll"
-assert CDEF_FILE.is_file(), f"CDEF file not found: {CDEF_FILE}"
-assert DLL_FILE.is_file(), f"DLL file not found: {DLL_FILE}"
 
 # Load C definitions and DLL
 ffi = cffi.FFI()
 with open(CDEF_FILE, "r") as cdef_file:
     cdef_content = cdef_file.read()
-    ffi.cdef(cdef_content)
+ffi.cdef(cdef_content)
 lib = ffi.dlopen(str(DLL_FILE))
-
-pass
---
+++ c/test_c_model.py
@@ -0,0 +1,131 @@
+"""General functionality testing of FIFO.
+
+This implementation fits both FIFO Python versions; FifoRef and Fifo.
+To be used as a template for the other implementations.
+"""
+
+# Standard library imports
+import os
+import unittest
+import random
+import time
+
+# Setup CFFI to load C FIFO implementation
+# This will compile the C code and load the resulting DLL
+from cffi_load import ffi, lib
+
+# Clean CLI
+time.sleep(3)   # Give some time for the user to read any compilation errors
+os.system("cls" if os.name == "nt" else "clear")
+
+# Project constants
+DEPTH = lib.DEPTH
+WLEN = 32
+MIN_VALUE = - 2 ** (WLEN - 1)
+MAX_VALUE = 2 ** (WLEN - 1) - 1
+
+# TODO: create a ERR component in C code to avoid using -1 as error code?
+
+class TestFifoPython(unittest.TestCase):
+    def setUp(self):
+        self.dut = lib.create_fifo()
+        self.dut_ptr = ffi.addressof(self.dut)
+
+    def test_read_back(self):
+        """Write and read back one value a time. Assert read values match."""
+        for _ in range(DEPTH + 2):
+            val = random.randint(MIN_VALUE, MAX_VALUE)
+            lib.write(self.dut_ptr, val)
+            dut_val = lib.read(self.dut_ptr)
+            self.assertEqual(dut_val, val)
+
+    def test_read_back_constant(self):
+        """Write a bulk of values, then read them all back. Assert read values match."""
+        values = [*range(DEPTH)]
+        for val in values:
+            dut_res = lib.write(self.dut_ptr, val)
+            self.assertNotEqual(dut_res, -1, msg=f"Failed to write value {val}.")
+        for idx, val in enumerate(values):
+            dut_val = lib.read(self.dut_ptr)
+            self.assertEqual(dut_val, val, msg=f"Read value mismatch at entry #{idx}.")
+
+    def test_overflow(self):
+        """Check if overflow raises BufferError."""
+        for idx in range(DEPTH):
+            dut_res = lib.write(self.dut_ptr, idx)
+            self.assertNotEqual(dut_res, -1, msg=f"Failed to write at position {idx}.")
+        dut_res = lib.write(self.dut_ptr, 100)
+        self.assertEqual(dut_res, -1)
+
+    def test_underflow(self):
+        """Check if underflow raises BufferError at startup and runtime."""
+        # test startup underflow
+        dut_res = lib.read(self.dut_ptr)
+        self.assertEqual(dut_res, -1)
+        # write some random values without overflowing it
+        values = [random.randint(MIN_VALUE, MAX_VALUE) for _ in range(random.randint(1, DEPTH - 1))]
+        for val in values:
+            dut_res = lib.write(self.dut_ptr, val)
+            self.assertNotEqual(dut_res, -1)
+        # read them all back
+        for _ in values:
+            lib.read(self.dut_ptr)
+        # test runtime underflow
+        dut_val = lib.read(self.dut_ptr)
+        self.assertEqual(dut_val, -1)
+
+    # TODO: add is_empty and is_full to C model
+    def test_under_and_overflow(self):
+        """Test a full cycle of filling and emptying the FIFO twice."""
+        for _ in range(2):
+            rnd_values = [random.randint(MIN_VALUE, MAX_VALUE) for _ in range(DEPTH + 3)]
+            rnd_values[DEPTH] = 123     # Make sure this one isn't -1
+            expetced_values = rnd_values[:DEPTH]
+            # Write up to one less than full
+            for val in expetced_values[:-1]:
+                dut_res = lib.write(self.dut_ptr, val)
+                self.assertNotEqual(dut_res, -1)
+                # self.assertFalse(self.dut.is_empty)
+                # self.assertFalse(self.dut.is_full)
+            # Insert last value to fill FIFO
+            dut_res = lib.write(self.dut_ptr, expetced_values[-1])
+            self.assertNotEqual(dut_res, -1)
+            # self.assertFalse(self.dut.is_empty)
+            # self.assertTrue(self.dut.is_full)
+            # Read back all expected values but one
+            for val in expetced_values[:-1]:
+                val_dut = lib.read(self.dut_ptr)
+                self.assertEqual(val_dut, val)
+                # self.assertFalse(self.dut.is_empty)
+                # self.assertFalse(self.dut.is_full)
+            # Read last expected value, emptying FIFO
+            val_dut = lib.read(self.dut_ptr)
+            self.assertEqual(val_dut, expetced_values[-1])
+            # self.assertTrue(self.dut.is_empty)
+            # self.assertFalse(self.dut.is_full)
+            # Test underflow
+            dut_res = lib.read(self.dut_ptr)
+            self.assertEqual(dut_res, -1)
+            # self.assertTrue(self.dut.is_empty)
+            # self.assertFalse(self.dut.is_full)
+
+    def test_value_bounds(self):
+        """Test that values written respect bounds and format."""
+        # Lower than minimum
+        with self.assertRaises(OverflowError):
+            dut_res = lib.write(self.dut_ptr, MIN_VALUE - 1)
+        # Higher than maximum
+        with self.assertRaises(OverflowError):
+            dut_res = lib.write(self.dut_ptr, MAX_VALUE + 1)
+        # Non-integer value
+        with self.assertRaises(TypeError):
+            lib.write(self.dut_ptr, 3.14)
+        # Valid: lower bound
+        dut_res = lib.write(self.dut_ptr, MIN_VALUE)
+        self.assertNotEqual(dut_res, -1)
+        # Valid: upper bound
+        dut_res = lib.write(self.dut_ptr, MAX_VALUE)
+        self.assertNotEqual(dut_res, -1)
+
+if __name__ == "__main__":
+    unittest.main(verbosity=0)
